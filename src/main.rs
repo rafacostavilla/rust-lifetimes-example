2573db590a2768b06c899d865dbd076e12906b12
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-16 14:55:05 +1200

    Create a struct with lifetime to hold a reference for a novel excerpt

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,21 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
     }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("Longest string");
     let string2 = "short string";
 
     let result = longest(string1.as_str(), &string2);
     println!("The longest string is '{result}'");
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt{
+        part: first_sentence,
+    };
+    println!("First sentence: '{}'", i.part);
 }
